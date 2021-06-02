b0419ab672c406625237e258e5f8c07a2b32a915
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-06-02 20:31:27 +0530

    api integration and functioning done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
-import video from '../src/videos/video1.mp4'
+import React, { useState } from 'react'
+import Axios from "axios";
 
 function App() {
+  const url = "https://api.adviceslip.com/advice";
+  const [advice, setadvice] = useState("Press the button below to get advice!!");
+  const getAdvice = async () => {
+    var result = await Axios.get(url);
+    console.log(result['data']['slip']['advice']);
+    setadvice(result['data']['slip']['advice']);
+  }
   return (
     <div className="app">
       <div className="card">
-        <h1>Headimgmn</h1>
-        <button type="submit">
+        <h1>{advice}</h1>
+        <button type="submit" onClick={getAdvice}>
           <span>GIVE ME ADVICE!</span>
         </button>
       </div>
